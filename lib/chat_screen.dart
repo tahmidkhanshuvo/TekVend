8b14efd6cc76bc745a021742768c1b3159eb2fa0
Tahmid Khan <tahmid.khan.shuvo@gmail.com>
2024-05-28 01:06:05 +0600

    Chat Screen deployments

---
+++ lib/chat_screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:intl/intl.dart';
+import 'message.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _userInput = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+
+  static const apiKey = 'AIzaSyBRP2wSZR4GiBja6wM-1L-miOvlzJbc5wg';
+
+  final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
+
+  final List<Message> _messages = [];
+
+  Future<void> sendMessage() async {
+    final message = _userInput.text;
+    if (message.trim().isEmpty) return;
+
+    setState(() {
+      _messages.add(Message(isUser: true, message: message, date: DateTime.now()));
+    });
+
+    final content = [Content.text(message)];
+    final response = await model.generateContent(content);
+
+    setState(() {
+      _messages.add(Message(isUser: false, message: response.text ?? "", date: DateTime.now()));
+    });
+
+    _userInput.clear();
+
+    // Ensure the scroll animation is completed after the new message is added
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            Image.asset(
+              'lib/images/chatlogo.png', // Update with your local image path
+              height: 120,
+            ),
+            const SizedBox(width: 10),
+            const Text(
+              'AI',
+              style: TextStyle(
+                fontSize: 29, // Adjust the font size as needed
+                fontWeight: FontWeight.bold, // Make the text bold
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.8), BlendMode.dstATop),
+            image: AssetImage('lib/images/chatbackground.png'), // Update with your local image path
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: NotificationListener<ScrollNotification>(
+                onNotification: (ScrollNotification scrollInfo) {
+                  // Handling scroll events, if necessary
+                  return false;
+                },
+                child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount: _messages.length,
+                  itemBuilder: (context, index) {
+                    final message = _messages[index];
+                    return Messages(
+                      isUser: message.isUser,
+                      message: message.message,
+                      date: DateFormat('HH:mm').format(message.date),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 15,
+                    child: TextFormField(
+                      style: const TextStyle(color: Colors.black),
+                      controller: _userInput,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        labelText: 'Ask Me Anything',
+                        labelStyle: const TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    padding: const EdgeInsets.all(12),
+                    iconSize: 30,
+                    onPressed: sendMessage,
+                    icon: const Icon(Icons.send, color: Colors.green),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
