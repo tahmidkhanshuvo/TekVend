ecceeac4e4875771f01e1bb9b74932e7085976a3
Tahmid Khan <tahmid.khan.shuvo@gmail.com>
2024-05-28 01:11:22 +0600

    Message ption added

---
+++ lib/message.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Message {
+  final bool isUser;
+  final String message;
+  final DateTime date;
+
+  Message({required this.isUser, required this.message, required this.date});
+}
+
+class Messages extends StatelessWidget {
+  final bool isUser;
+  final String message;
+  final String date;
+
+  const Messages({
+    super.key,
+    required this.isUser,
+    required this.message,
+    required this.date,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(15),
+      margin: EdgeInsets.symmetric(vertical: 10).copyWith(
+        left: isUser ? 100 : 10,
+        right: isUser ? 10 : 100,
+      ),
+      decoration: BoxDecoration(
+        color: isUser ? Colors.green : Colors.grey.shade400,
+        borderRadius: BorderRadius.only(
+          topLeft: const Radius.circular(10),
+          bottomLeft: isUser ? const Radius.circular(10) : Radius.zero,
+          topRight: const Radius.circular(10),
+          bottomRight: isUser ? Radius.zero : const Radius.circular(10),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            message,
+            style: TextStyle(
+              fontSize: 16,
+              color: isUser ? Colors.white : Colors.black,
+            ),
+          ),
+          const SizedBox(height: 5), // Space between message and date
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Text(
+              date,
+              style: TextStyle(
+                fontSize: 10,
+                color: isUser ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
