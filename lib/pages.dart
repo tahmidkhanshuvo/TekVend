35b9a6142f27b4f8d7a60e13dd843c0028316b8a
Tahmid Khan <tahmid.khan.shuvo@gmail.com>
2024-06-24 21:09:44 +0600

    pages.dart added

---
+++ lib/pages.dart
@@ -0,0 +1,28 @@
+
+//
+export 'user profile/profile.dart';
+export 'shopping/cart.dart';
+export 'images/images.dart';
+export 'home page/chat_screen.dart';
+export 'home page/product_page.dart';
+
+export 'controllers/message.dart';
+
+export 'auth screen/sign_up_screen.dart';
+export 'auth screen/sign_in_screen.dart';
+export 'auth screen/otp_page.dart';
+
+
+
+
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:flutter/gestures.dart';
+export 'package:flutter/material.dart';
+export 'package:google_sign_in/google_sign_in.dart';
+export 'package:google_generative_ai/google_generative_ai.dart';
+export 'package:intl/intl.dart';
+export 'package:amar/firebase_options.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:flutter/services.dart';
+
+
